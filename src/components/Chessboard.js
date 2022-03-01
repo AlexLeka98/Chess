21ce9e4de2f7f07ba5afb044bb5f9edfad13c3bd
AlexLeka98 <alexleka98@outlook.com>
2022-03-01 16:35:27 +0100

    Basic board has been made

--- src/components/Chessboard.js
+++ src/components/Chessboard.js
@@ -10,10 +10,36 @@ const Chessboard = () => {
     for (let i = 7; i >= 0; i--) {
         for (let j = 0; j <= 7; j++) {
             if ((i + j) % 2 === 0) {
-                board.push(<span className={styles.whiteBox}>{`${horizontalAxis[j]}${verticalAxis[i]}`}</span>)
+                if (j === 0 && i === 0) {
+                    board.push(<span className={styles.whiteBox}>
+                        <div>{verticalAxis[i]}</div>
+                        <div>{horizontalAxis[j]}</div>
+                    </span>);
+                }
+                else if (j === 0) {
+                    board.push(<span className={styles.whiteBox}>{verticalAxis[i]}</span>);
+                }
+                else if (i === 0) {
+                    board.push(<span className={styles.whiteBox}>
+                        <div style={{width:"100%"}}>{horizontalAxis[j]}</div>
+                        </span>);
+                }
+                else {
+                    board.push(<span className={styles.whiteBox}></span>);
+                }
             }
-            else{
-                board.push(<span>{`${horizontalAxis[j]}${verticalAxis[i]}`}</span>)
+            else {
+                if (j === 0) {
+                    board.push(<span>{verticalAxis[i]}</span>);
+                }
+                else if (i === 0) {
+                    board.push(<span>
+                        <div style={{width:"100%"}}>{horizontalAxis[j]}</div>
+                    </span>);
+                }
+                else {
+                    board.push(<span></span>);
+                }
             }
         }
     }
